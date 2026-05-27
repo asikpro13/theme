8899d29a45f98b312ee36329ba43dabad2ac11cb
Vadim Mazurek <asikpro13@mail.ru>
2026-05-27 23:48:28 +0300

    theme and colors

---
+++ lib/appcolors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+// Класс отвечает за основные цвета приложения
+// Автор создания: 2
+// Дата создания: 27.05.2026
+class AppColors {
+  // Цвет primary с макета
+  static const Color primary = Color(0xff004AC6);
+
+  // Цвет secondary с макета
+  static const Color secondary = Color(0xff505F76);
+
+  // Цвет tertiary с макета
+  static const Color tertiary = Color(0xff943700);
+
+  // Цвет error с макета
+  static const Color error = Color(0xffBA1A1A);
+
+  // Цвет white с макета
+  static const Color white = Color(0xffFFFFFF);
+
+  // Цвет black с макета
+  static const Color black = Color(0xff191B23);
+}
---
+++ lib/apptheme.dart
@@ -0,0 +1,38 @@
+import 'package:apptextstyle/appcolors.dart';
+import 'package:flutter/material.dart';
+import 'package:vize/vize.dart';
+
+// Расширение отвечает за добавление текстовых стилей в тему приложения
+// Автор создания: 2
+// Дата создания: 27.05.2026
+extension TextStyleExtension on TextTheme {
+  // Текстовый стиль screenHeader с макета
+  TextStyle get screenHeader {
+    return TextStyle(fontFamily: 'Manrope', fontWeight: FontWeight.w700, fontSize: 24.fh, height: 32.fh / 24.fh, letterSpacing: -0.48.fw, color: AppColors.black);
+  }
+
+  // Текстовый стиль subheader с макета
+  TextStyle get subheader {
+    return TextStyle(fontFamily: 'Manrope', fontWeight: FontWeight.w600, fontSize: 18.fh, height: 24.fh / 18.fh, letterSpacing: -0.18.fw, color: AppColors.black);
+  }
+
+  // Текстовый стиль bodyMedium с макета
+  TextStyle get bodyMedium {
+    return TextStyle(fontFamily: 'Manrope', fontWeight: FontWeight.w400, fontSize: 16.fh, height: 24.fh / 16.fh, letterSpacing: 0.fw, color: AppColors.black);
+  }
+
+  // Текстовый стиль bodySmall с макета
+  TextStyle get bodySmall {
+    return TextStyle(fontFamily: 'Manrope', fontWeight: FontWeight.w400, fontSize: 14.fh, height: 20.fh / 14.fh, letterSpacing: 0.fw, color: AppColors.black);
+  }
+
+  // Текстовый стиль fieldLabel с макета
+  TextStyle get fieldLabel {
+    return TextStyle(fontFamily: 'Manrope', fontWeight: FontWeight.w600, fontSize: 14.fh, height: 20.fh / 14.fh, letterSpacing: 0.fw, color: AppColors.black);
+  }
+
+  // Тестовый текстовый стиль
+  TextStyle get testStyle {
+    return TextStyle(fontFamily: 'Playwrite', fontWeight: FontWeight.w400, fontSize: 14.fh, height: 20.fh / 14.fh, letterSpacing: 0.fw, color: AppColors.black);
+  }
+}
